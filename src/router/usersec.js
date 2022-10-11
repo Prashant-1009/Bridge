daf65bc9cf18554636c5e1aa360c1efc0b68c7b4
Prashant Singh <prashant1009sh@gmail.com>
2022-10-11 20:31:18 +0530

    adding website details

--- src/router/usersec.js
+++ src/router/usersec.js
@@ -49,13 +49,13 @@ router.post("/login", async (req, res) => {
             if (details.password === interedpassword) 
             {
 
-                        const reqdata = await axios.get(process.env.fapi+'/api/yourreqdata', {
+                        const reqdata = await axios.get('https://bridgesrmgpc.herokuapp.com'+'/api/yourreqdata', {
                             params: {
                                 rollno: interedrollno
                             }
                         })
                   
-                        const matchedreq = await axios.get(process.env.fapi+'/api/matchedreqdata', {
+                        const matchedreq = await axios.get('https://bridgesrmgpc.herokuapp.com'+'/api/matchedreqdata', {
                             params: {
                                 rollno: interedrollno,
                                 course: details.course,
@@ -66,14 +66,14 @@ router.post("/login", async (req, res) => {
                         })
 
 
-                        const prodata = await axios.get(process.env.fapi+'/api/yourproductdata', {
+                        const prodata = await axios.get('https://bridgesrmgpc.herokuapp.com'+'/api/yourproductdata', {
                             params: {
                                 rollno: interedrollno
                             }
                         })
 
 
-                        const matchedpro = await axios.get(process.env.fapi+'/api/matchedprodata', {
+                        const matchedpro = await axios.get('https://bridgesrmgpc.herokuapp.com'+'/api/matchedprodata', {
                             params: {
                                 rollno: interedrollno,
                                 course: details.course,
